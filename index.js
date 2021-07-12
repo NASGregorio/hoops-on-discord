0fda01efeeeec5660afe2e7ae35025ac58c25267
Nelson Gregorio <nelson.gregorio@live.com.pt>
2021-07-12 23:10:11 +0100

    feat: working proof of concept

---
+++ index.js
@@ -0,0 +1,53 @@
+const Simulation = require("./simulation");
+const Discord = require("discord.js");
+require('dotenv').config();
+
+const client = new Discord.Client();
+
+let sim = Simulation.init();
+
+const states = {
+  READY: "READY",
+  SHOOT: "SHOOT",
+  HOLD: "HOLD",
+};
+let state = states.READY;
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", (msg) => {
+  if (msg.content === "!h ready") {
+    if (state != states.READY) return;
+
+    state = states.SHOOT;
+    sim.resetBall();
+    sim.render();
+    Simulation.captureSingleFrame(sim.canvas);
+    msg.channel.send({
+      files: ["./setup.png"],
+    });
+  } else if (msg.content.startsWith("!h shoot")) {
+    if (state != states.SHOOT) return;
+    state = states.HOLD;
+    let parts = msg.content.split(" ");
+    if (parts.length == 4) {
+      const mag = parseInt(parts[2]);
+      if (mag > 50) {
+        msg.reply("not so fast buddy!");
+        state = states.SHOOT;
+        return;
+      }
+      sim.prepare(parseInt(parts[2]), parseInt(parts[3]));
+      Simulation.runSimulation(sim, () => {
+        msg.channel.send({
+          files: ["./result.gif"],
+        });
+        state = states.READY;
+      });
+    }
+  }
+});
+
+client.login(process.env.Hoops2DToken);
---
+++ renderer.js
@@ -0,0 +1,47 @@
+module.exports = class Render {
+  constructor(width, height, canvas, engine) {
+    this.width = width;
+    this.height = height;
+
+    this.engine = engine;
+
+    this.canvas = canvas;
+    this.ctx = canvas.getContext("2d");
+
+    this.entities = [];
+  }
+
+  testme() {
+    console.log(this.width);
+  }
+
+  addEntity(vertices, stroke, fill) {
+    this.entities.push({ vertices: vertices, stroke: stroke, fill: fill });
+  }
+
+  render() {
+    // background
+    this.ctx.fillStyle = "#252525";
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+    this.entities.forEach((entity) => {
+      this.ctx.lineWidth = 1;
+
+      this.ctx.beginPath();
+      this.ctx.moveTo(entity.vertices[0].x, entity.vertices[0].y);
+
+      for (let j = 1; j < entity.vertices.length; j++) {
+        this.ctx.lineTo(entity.vertices[j].x, entity.vertices[j].y);
+      }
+
+      this.ctx.lineTo(entity.vertices[0].x, entity.vertices[0].y);
+      this.ctx.closePath();
+
+      this.ctx.strokeStyle = entity.stroke;
+      this.ctx.stroke();
+
+      this.ctx.fillStyle = entity.fill;
+      this.ctx.fill();
+    });
+  }
+};
